24f06fa8f30128762c5215a156cdaeea5bc06ca3
Ryan Kozin <ryankozin@Ryans-MacBook-Pro.local>
2023-07-13 14:32:33 -0700

    completed lab and added view more button to add more ships to list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import getAllStarships, {getNextPage} from "./services/sw-api.js";
+import React, { useState, useEffect } from "react";
+import StarShipCard from "./StarShipCard.js";
+import './style.css';
 
-function App() {
+export default function App() {
+  const [shipList, setShipList] = useState([]);
+  const [result, setResult] = useState([]);
+
+  async function handleAddShips(){
+    if (shipList.length < result.count){
+    const nextPageResult = await getNextPage(result);
+    console.log(nextPageResult);
+    setResult(nextPageResult);
+    const newShips = nextPageResult.results
+    console.log([...shipList, ...newShips]);
+    setShipList([...shipList, ...newShips]);
+    } else {
+    alert('All ships listed successfully')
+    }
+  }
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const result = await getAllStarships();
+        const ships = result.results
+        console.log(result);
+        setResult(result);
+        setShipList(ships);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    {shipList.map((ship) => (
+      <StarShipCard name={ship.name} cost_in_credits={ship.cost_in_credits}/>
+      ))}
+      <button onClick={handleAddShips}>View More</button>
     </div>
   );
 }
-
-export default App;
